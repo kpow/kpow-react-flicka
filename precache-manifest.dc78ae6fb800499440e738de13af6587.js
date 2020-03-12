d05ac7ba8a032015cbabf9a6e543515544c160e4
Kevin Power <kpow@kpow.com>
2020-03-12 08:37:44 -0400

    Updates

---
+++ precache-manifest.dc78ae6fb800499440e738de13af6587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7eb667b0e7f94006cd265c25b142ac5",
+    "url": "/react-gallery/index.html"
+  },
+  {
+    "revision": "6d7f0d3eed4b8c9d8b35",
+    "url": "/react-gallery/static/css/main.e1ce1a3a.chunk.css"
+  },
+  {
+    "revision": "7dfab324d0d75a9b475a",
+    "url": "/react-gallery/static/js/2.e9246feb.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-gallery/static/js/2.e9246feb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d7f0d3eed4b8c9d8b35",
+    "url": "/react-gallery/static/js/main.f217c8da.chunk.js"
+  },
+  {
+    "revision": "cd7cc58c11c59df1dd0f",
+    "url": "/react-gallery/static/js/runtime-main.30d545b9.js"
+  }
+]);
